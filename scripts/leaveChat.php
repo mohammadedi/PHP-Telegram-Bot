a5691b510b7285df4b0978e531c3a0af824ba8d9
root <root@ns3307820.ip-178-32-220.eu>
2017-09-20 19:40:35 +0300

    Fixed leaveChat

--- scripts/leaveChat.php
+++ scripts/leaveChat.php
@@ -1,9 +1,10 @@
 <?php
+# если создатель бота в чате напишет "бот уходи", бот обидится и покинет чат 
 if($_USER['username'] == ADMIN)
 	{
 		if($_CHAT['id'] != $_INFO['id'])
 			{
-				if(preg_match("#ада уходи#iu", $_TEXT))
+				if(preg_match("#бот уходи#iu", $_TEXT))
 					{
 						sendMessage($_CHAT['id'], 'Okay :(');
 						sleep(3);
